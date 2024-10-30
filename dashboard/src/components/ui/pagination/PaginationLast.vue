ee169cfd97c607a2f194f078084df7348d5e9014
wan azib <incogmy@gmail.com>
2024-10-31 01:38:36 +0800

    latest

--- dashboard/src/components/ui/pagination/PaginationLast.vue
+++ dashboard/src/components/ui/pagination/PaginationLast.vue
@@ -4,7 +4,7 @@ import { PaginationLast, type PaginationLastProps } from 'radix-vue'
 import { ChevronsRight } from 'lucide-vue-next'
 import {
   Button,
-} from '/src/components/ui/button'
+} from '@/components/ui/button'
 import { cn } from '@/lib/utils'
 
 const props = withDefaults(defineProps<PaginationLastProps & { class?: HTMLAttributes['class'] }>(), {
